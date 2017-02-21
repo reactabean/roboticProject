d0076865ebc7d427543ea8a60c1118c3c6e36731
AFettes <adrian.fettes@gmail.com>
2017-02-21 12:52:19 -0800

    Commit constants file

---
+++ Constants.h
@@ -0,0 +1,23 @@
+//link Paramater limits
+#define HIGHTHEATA1	    150
+#define LOWTHEATA1     -150
+
+#define HIGHTHEATA2	    100
+#define LOWTHEATA12	   -100
+
+#define HIGHDISTANCE3  -100 
+#define LOWDISTANCE3   -200 
+
+#define HIGHTHEATA4	    160
+#define LOWTHEATA4 	   -160
+
+//Define structure parameters
+#define L1 405
+#define L2 70
+#define L3 195
+#define L4 142
+#define L5 140
+#define L6 80
+#define L7 130
+#define L8 10
+#define Lmax 410
