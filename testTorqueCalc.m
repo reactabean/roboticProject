d979473548822136d72c17cf551accbbdc3b39bd
AFettes <adrian.fettes@gmail.com>
2017-04-04 19:00:22 -0700

    Fixed typo in torque testing

--- testTorqueCalc.m
+++ testTorqueCalc.m
@@ -3,7 +3,7 @@ syms M1 M2 L1 L2 theta1 theta2 thetadot1 thetadot2 thetadotdot1 thetadotdot2;
 R1 = [cos(theta1), -sin(theta1), 0; sin(theta1), cos(theta1), 0; 0, 0, 1];
 R2 = [cos(theta2), -sin(theta2), 0; sin(theta2), cos(theta2), 0; 0, 0, 1];
 rotationMatrices(:, :, 1) = R1;
-rotationMatrices(:, :, 1) = R2;
+rotationMatrices(:, :, 2) = R2;
 
 jointVelocities = [thetadot1, thetadot2; 0 0; 0 0];
 jointAccelerations = [thetadotdot1, thetadotdot2; 0 0; 0 0];
